f10ed7ca455a43878d0ac110e74e2af5723481a3
MAITREYEE DINGARE <maitreyeedingare36@gmail.com>
2019-07-21 13:38:32 +0200

    Update DecionTreeImplementation.py

--- DecionTreeImplementation.py
+++ DecionTreeImplementation.py
@@ -93,7 +93,7 @@ lr_test_score=lr.score(X_test,y_test)
 print ("linear regression test score: ", lr_test_score)
 
 
-""" # Printing the graph
+# Printing the graph
 from sklearn.externals.six import StringIO  
 from IPython.display import Image  
 from sklearn.tree import export_graphviz
@@ -105,4 +105,4 @@ export_graphviz(regr_2, out_file=dot_data,
                 special_characters=True, feature_names = feature_cols,class_names=['0','1'])
 graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
 graph.write_png('diabetes.png')
-Image(graph.create_png()) """
+Image(graph.create_png()) 
